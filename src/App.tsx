ffec279bcf7b4f8048c50086720dc9cb5f35a395
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-01 21:07:30 -0500

    Add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-red-300 rounded border-2'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
