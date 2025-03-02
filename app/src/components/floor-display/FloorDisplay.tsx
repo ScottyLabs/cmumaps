1fbff6c2a5384f5a2ed7f0ed96208d68da83d02f
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-02 16:24:15 -0500

    Add minimal FloorDisplay that can allows panning and zooming of pdf

---
+++ app/src/components/floor-display/FloorDisplay.tsx
@@ -0,0 +1,68 @@
+import Konva from 'konva';
+
+import { Stage, Layer } from 'react-konva';
+
+// import { useAppDispatch, useAppSelector } from '../../store/hooks';
+// import { selectEditPolygon } from '../../store/slices/modeSlice';
+// import { getNodeIdSelected } from '../../store/slices/mouseEventSlice';
+import { PDFCoordinate } from '../shared/types';
+
+interface Props {
+  floorCode: string;
+  setCanPan: (canPan: boolean) => void;
+  handleWheel: (evt: Konva.KonvaEventObject<WheelEvent>) => void;
+  handleDragMove: (evt: Konva.KonvaEventObject<DragEvent>) => void;
+  scale: number;
+  offset: PDFCoordinate;
+  stageRef: React.RefObject<Konva.Stage | null>;
+}
+
+const FloorDisplay = ({
+  // floorCode,
+  setCanPan,
+  handleWheel,
+  handleDragMove,
+  scale,
+  offset,
+  stageRef,
+}: Props) => {
+  // const dispatch = useAppDispatch();
+
+  // const mode = useAppSelector((state) => state.mode.mode);
+  // const nodeIdSelected = useAppSelector((state) =>
+  //   getNodeIdSelected(state.mouseEvent),
+  // );
+
+  // const showOutline = useAppSelector((state) => state.visibility.showOutline);
+  // const showNodes = useAppSelector((state) => state.visibility.showNodes);
+  // const showEdges = useAppSelector((state) => state.visibility.showEdges);
+  // const showPolygons = useAppSelector((state) => state.visibility.showPolygons);
+
+  // const editPolygon = useAppSelector(selectEditPolygon);
+  // const editRoomLabel = useAppSelector((state) => state.ui.editRoomLabel);
+
+  return (
+    <>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        // onMouseMove={handleMouseMove}
+        // onMouseDown={handleOnMouseDown}
+        onMouseUp={() => setCanPan(true)}
+        // onClick={handleStageClick}
+        onWheel={handleWheel}
+        onDragMove={handleDragMove}
+        draggable
+        scaleX={scale}
+        scaleY={scale}
+        x={offset.x}
+        y={offset.y}
+        ref={stageRef}
+      >
+        <Layer></Layer>
+      </Stage>
+    </>
+  );
+};
+
+export default FloorDisplay;
