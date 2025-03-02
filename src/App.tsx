b2da883d274c944671ba92c1655c539efabf9aae
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-01 21:33:29 -0500

    Add tsc check in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
