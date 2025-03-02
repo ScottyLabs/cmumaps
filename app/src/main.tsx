b34a0e5ea3e32dbfaa7920a7d09d27b49aaec41f
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-02 03:17:19 -0500

    Able to protect all routes with Clerk
    Finally figured out after an hour of struggle on deprecated React Router documentations 😭
    
    Kinda miss SSR now...

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,13 @@
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider, RedirectToSignIn, useUser } from '@clerk/clerk-react';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router';
 
 import './index.css';
-import AppRouter from './pages/AppRouter.tsx';
+import FloorDisplay from './pages/FloorDisplay.tsx';
+import Home from './pages/Home.tsx';
 import { store } from './store/store.ts';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -15,12 +16,27 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file');
 }
 
+const ProtectedRoute = () => {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
+  }
+
+  return <Outlet />;
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <BrowserRouter>
         <Provider store={store}>
-          <AppRouter />
+          <Routes>
+            <Route element={<ProtectedRoute />}>
+              <Route index element={<Home />} />
+              <Route path=":floorCode" element={<FloorDisplay />} />
+            </Route>
+          </Routes>
         </Provider>
       </BrowserRouter>
     </ClerkProvider>
