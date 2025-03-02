75463df1fce363a5cf419eb4c7d3e602b56db006
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-01 22:46:04 -0500

    Able to query database with Prisma in server

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  const firstBuilding = await prisma.building.findFirst();
+  res.json(firstBuilding);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit();
 });
