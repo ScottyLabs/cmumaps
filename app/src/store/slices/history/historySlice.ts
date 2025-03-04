06666cc6e6d24acd1462252fc3efb3fa184ffe26
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-04 13:41:14 -0500

    Separate history thunks into its own file

--- app/src/store/slices/history/historySlice.ts
+++ app/src/store/slices/history/historySlice.ts
@@ -1,11 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { toast } from "react-toastify";
-
-import { nodeApiSlice } from "../../api/nodeApiSlice";
-import { AppDispatch } from "../../store";
-import { createAppAsyncThunk } from "../../withTypes";
-import { Edit, EditPair } from "../historyTypes";
+import { redo, undo } from "./historyThunks";
+import { Edit, EditPair } from "./historyTypes";
 
 const MAX_UNDO_LIMIT = 50;
 
@@ -20,59 +16,6 @@ const initialState: HistoryState = {
   reversedEditHistory: [],
   editIndex: -1,
 };
-
-const applyEdit = (edit: Edit, dispatch: AppDispatch) => {
-  switch (edit.endpoint) {
-    case "createNode":
-      dispatch(nodeApiSlice.endpoints.createNode.initiate(edit.arg)).unwrap();
-      break;
-    case "deleteNode":
-      dispatch(nodeApiSlice.endpoints.deleteNode.initiate(edit.arg)).unwrap();
-      break;
-  }
-};
-
-export const undo = createAppAsyncThunk(
-  "history/undo",
-  (_, { dispatch, getState }) => {
-    try {
-      const historyState = getState().history;
-      const editIndex = historyState.editIndex;
-      if (editIndex == -1) {
-        toast.warn("Can't undo anymore!");
-        return Promise.reject();
-      }
-      // apply the reversed edit
-      const reversedEdit = historyState.reversedEditHistory[editIndex];
-      applyEdit(reversedEdit, dispatch);
-    } catch (error) {
-      toast.error("Failed to undo change!");
-      console.error("Error undoing:", error);
-      return Promise.reject();
-    }
-  },
-);
-
-export const redo = createAppAsyncThunk(
-  "history/redo",
-  (_, { dispatch, getState }) => {
-    try {
-      const historyState = getState().history;
-      const editIndex = historyState.editIndex;
-      if (editIndex === historyState.editHistory.length) {
-        toast.warn("Can't redo anymore!");
-        return Promise.reject();
-      }
-      // apply the edit
-      const edit = historyState.editHistory[editIndex];
-      applyEdit(edit, dispatch);
-    } catch (error) {
-      toast.error("Failed to redo change!");
-      console.error("Error redoing:", error);
-      return Promise.reject();
-    }
-  },
-);
 
 const getUpdatedHistory = (history: Edit[], edit: Edit, index: number) => {
   const updatedHistory = [...history.slice(0, index + 1), edit];
---
+++ app/src/store/slices/history/historyThunks.ts
@@ -0,0 +1,59 @@
+import { toast } from "react-toastify";
+
+import { nodeApiSlice } from "../../api/nodeApiSlice";
+import { AppDispatch } from "../../store";
+import { createAppAsyncThunk } from "../../withTypes";
+import { Edit } from "./historyTypes";
+
+const applyEdit = (edit: Edit, dispatch: AppDispatch) => {
+  switch (edit.endpoint) {
+    case "createNode":
+      dispatch(nodeApiSlice.endpoints.createNode.initiate(edit.arg)).unwrap();
+      break;
+    case "deleteNode":
+      dispatch(nodeApiSlice.endpoints.deleteNode.initiate(edit.arg)).unwrap();
+      break;
+  }
+};
+
+export const undo = createAppAsyncThunk(
+  "history/undo",
+  (_, { dispatch, getState }) => {
+    try {
+      const historyState = getState().history;
+      const editIndex = historyState.editIndex;
+      if (editIndex == -1) {
+        toast.warn("Can't undo anymore!");
+        return Promise.reject();
+      }
+      // apply the reversed edit
+      const reversedEdit = historyState.reversedEditHistory[editIndex];
+      applyEdit(reversedEdit, dispatch);
+    } catch (error) {
+      toast.error("Failed to undo change!");
+      console.error("Error undoing:", error);
+      return Promise.reject();
+    }
+  },
+);
+
+export const redo = createAppAsyncThunk(
+  "history/redo",
+  (_, { dispatch, getState }) => {
+    try {
+      const historyState = getState().history;
+      const editIndex = historyState.editIndex;
+      if (editIndex === historyState.editHistory.length) {
+        toast.warn("Can't redo anymore!");
+        return Promise.reject();
+      }
+      // apply the edit
+      const edit = historyState.editHistory[editIndex];
+      applyEdit(edit, dispatch);
+    } catch (error) {
+      toast.error("Failed to redo change!");
+      console.error("Error redoing:", error);
+      return Promise.reject();
+    }
+  },
+);
