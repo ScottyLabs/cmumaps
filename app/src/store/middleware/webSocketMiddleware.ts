53e63f4d44f82604422968a6a7ad1346e64f0e7b
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-04 01:36:49 -0500

    Recalculate floorCode when handling create-node websocket event

--- app/src/store/middleware/webSocketMiddleware.ts
+++ app/src/store/middleware/webSocketMiddleware.ts
@@ -22,8 +22,13 @@ interface ParamsType {
   dispatch: AppDispatch;
 }
 
-const createSocket = (floorCode: string | undefined, dispatch: AppDispatch) => {
-  // Create new socket connection
+// Get floor code from URL
+const getFloorCode = () => {
+  return window.location.pathname.split("/")[1];
+};
+
+// Create socket connection
+const createSocket = (dispatch: AppDispatch) => {
   const socket = io(import.meta.env.VITE_SERVER_URL);
 
   socket.on("connect", () => {
@@ -34,9 +39,11 @@ const createSocket = (floorCode: string | undefined, dispatch: AppDispatch) => {
     console.log("Disconnected from server");
   });
 
+  // Handle create node event
   socket.on<WebSocketEventType>(
     WebSocketEvents.CREATE_NODE,
     (message: WebSocketPayloads["create-node"]) => {
+      const floorCode = getFloorCode();
       if (!floorCode) {
         return;
       }
@@ -61,13 +68,13 @@ const createSocket = (floorCode: string | undefined, dispatch: AppDispatch) => {
 const webSocketMiddleware: Middleware = (params) => (next) => (action) => {
   const { dispatch } = params as ParamsType;
   const { type } = action as Action;
-  const floorCode = window.location.pathname.split("/")[1];
+  const floorCode = getFloorCode();
 
   switch (type) {
     case WEBSOCKET_JOIN: {
       // Connect to socket
       if (socket === null) {
-        socket = createSocket(floorCode, dispatch);
+        socket = createSocket(dispatch);
       }
 
       // Join room
