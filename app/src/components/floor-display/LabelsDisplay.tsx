57f1c920b3077a0049d4acfe3de9765fda9dd20b
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-09 03:56:04 -0400

    Able to edit room label

--- app/src/components/floor-display/LabelsDisplay.tsx
+++ app/src/components/floor-display/LabelsDisplay.tsx
@@ -1,9 +1,14 @@
+import Konva from "konva";
+import { v4 as uuidv4 } from "uuid";
+
 import { useMemo } from "react";
 import { TfiLocationPin } from "react-icons/tfi";
 import { Group, Path, Rect, Text } from "react-konva";
 import { useNavigate } from "react-router";
 
-import { Graph, Rooms } from "../../../../shared/types";
+import { Graph, RoomInfo, Rooms } from "../../../../shared/types";
+import useValidatedFloorParams from "../../hooks/useValidatedFloorParams";
+import { useUpdateRoomMutation } from "../../store/api/roomApiSlice";
 import { useAppSelector } from "../../store/hooks";
 import { setCursor } from "../../utils/canvasUtils";
 
@@ -13,10 +18,13 @@ interface Props {
   rooms: Rooms;
 }
 
-const LabelsDisplay = ({ rooms }: Props) => {
+const LabelsDisplay = ({ floorCode, rooms }: Props) => {
   const navigate = useNavigate();
+  const { roomId: selectedRoomId } = useValidatedFloorParams(floorCode);
 
-  // const editRoomLabel = useAppSelector((state) => state.ui.editRoomLabel);
+  const [updateRoom] = useUpdateRoomMutation();
+
+  const editRoomLabel = useAppSelector((state) => state.ui.editRoomLabel);
   const showLabels = useAppSelector((state) => state.visibility.showLabels);
 
   // label icon
@@ -37,41 +45,37 @@ const LabelsDisplay = ({ rooms }: Props) => {
   return Object.entries(rooms).map(([roomId, room]) => {
     // selected if it is edit label mode and it is the label of the selected room
     // or the room of the label is connected by the selected door
-    // const selected =
-    //   (editRoomLabel && roomIdSelected == roomId) ||
+    const selected = editRoomLabel && selectedRoomId == roomId;
     //   (idSelected.type == DOOR &&
     //     doors[idSelected.id].roomIds.includes(roomId));
 
-    const selected = false;
+    const draggable = editRoomLabel && selected;
 
     // don't show label when not showing all labels and not selected
-    if (!showLabels) {
+    if (!showLabels && !selected) {
       return;
     }
 
-    // const draggable = editRoomLabel && roomIdSelected == roomId;
-
-    // const handleOnDragEnd = (e) => {
-    //   const oldRoom = rooms[roomId];
-    //   const newRoom: RoomInfo = {
-    //     ...oldRoom,
-    //     labelPosition: {
-    //       x: Number((e.target.x() + width / 2).toFixed(2)),
-    //       y: Number((e.target.y() + height).toFixed(2)),
-    //     },
-    //   };
-    //   upsertRoom({ floorCode, roomId, newRoom, oldRoom });
-    // };
+    const handleOnDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
+      const roomInfo: Partial<RoomInfo> = {
+        labelPosition: {
+          x: Number((e.target.x() + width / 2).toFixed(2)),
+          y: Number((e.target.y() + height).toFixed(2)),
+        },
+      };
+      updateRoom({ floorCode, roomId, roomInfo, batchId: uuidv4() });
+    };
 
     return (
       <Group
         x={room.labelPosition.x - width / 2}
         y={room.labelPosition.y - height}
         key={roomId}
-        // draggable={draggable}
         onMouseEnter={(e) => setCursor(e, "pointer")}
         onMouseLeave={(e) => setCursor(e, "default")}
         onClick={() => navigate(`?roomId=${roomId}`)}
+        draggable={draggable}
+        onDragEnd={handleOnDragEnd}
       >
         <Path fill={selected ? "orange" : "indigo"} data={path} />
         <Rect width={Number(width)} height={Number(height)} />
