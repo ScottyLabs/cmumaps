e05ea3fc97f8a2801d135ffb384cf5911bb027f6
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-28 19:06:19 -0400

    Able to display the Apple Map base

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,56 @@
-import { useEffect, useState } from "react";
+import { FeatureVisibility, Map, MapType } from "mapkit-react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import { useRef } from "react";
 
-  useEffect(() => {
-    if (count === 0) {
-      setCount(1);
-    }
-  }, []);
+// CMU Campus
+const INITIAL_REGION = {
+  centerLatitude: 40.444,
+  centerLongitude: -79.945,
+  latitudeDelta: 0.006337455593801167,
+  longitudeDelta: 0.011960061265583022,
+};
+
+const CAMERA_BOUNDARY = {
+  centerLatitude: 40.44533940432823,
+  centerLongitude: -79.9457060010195,
+  latitudeDelta: 0.009258427149788417,
+  longitudeDelta: 0.014410141520116326,
+};
+
+const App = () => {
+  const mapRef = useRef<mapkit.Map | null>(null);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </>
+    <main className="relative h-screen">
+      <Map
+        ref={mapRef}
+        token={import.meta.env.VITE_MAPKIT_TOKEN || ""}
+        initialRegion={INITIAL_REGION}
+        includedPOICategories={[]}
+        cameraBoundary={CAMERA_BOUNDARY}
+        minCameraDistance={5}
+        maxCameraDistance={1500}
+        showsUserLocationControl
+        showsUserLocation={true}
+        mapType={MapType.MutedStandard}
+        // paddingBottom={isMobile ? 72 : 0}
+        paddingBottom={0}
+        paddingLeft={4}
+        paddingRight={4}
+        paddingTop={10}
+        // showsZoomControl={!isMobile}
+        showsCompass={FeatureVisibility.Visible}
+        allowWheelToZoom
+        // onRegionChangeStart={onRegionChangeStart}
+        // onRegionChangeEnd={() => {
+        //   dispatch(setIsZooming(false));
+        //   onRegionChangeEnd();
+        // }}
+        // onClick={handleClick}
+        // onLoad={handleLoad}
+      ></Map>
+    </main>
   );
-}
+};
 
 export default App;
