4fe80b783336e0560ea27496ec71fd199457a7bf
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-28 18:35:23 -0400

    Add redux
    https://redux.js.org/introduction/installation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App.tsx";
 import "./index.css";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
