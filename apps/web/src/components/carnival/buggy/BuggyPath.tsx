4880174489505a1d87254858e77ba38c3e61fa11
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-29 19:47:40 -0400

    Update database schema

--- apps/web/src/components/carnival/buggy/BuggyPath.tsx
+++ apps/web/src/components/carnival/buggy/BuggyPath.tsx
@@ -2,7 +2,7 @@ import buggyPath from "@public/assets/carnival/json/buggy-path.json";
 import { Polyline } from "mapkit-react";
 
 const BuggyPath = () => {
-  return <Polyline points={buggyPath} strokeColor="red" lineWidth={3} />;
+  return <Polyline points={buggyPath} strokeColor="red" lineWidth={6} />;
 };
 
 export default BuggyPath;
