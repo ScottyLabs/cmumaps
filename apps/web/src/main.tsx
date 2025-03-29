a98554bd45afd0b4009420833c0eb2da37560519
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-29 00:30:00 -0400

    Add Clerk to web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,3 +1,6 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-react";
+import { Clerk } from "@clerk/types";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -7,12 +10,30 @@ import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store/store.ts";
 
+// https://clerk.com/docs/components/control/clerk-loaded
+declare global {
+  interface Window {
+    Clerk: Clerk;
+  }
+}
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkLoaded>
+      <BrowserRouter>
+        <StrictMode>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </StrictMode>
+      </BrowserRouter>
+    </ClerkLoaded>
+  </ClerkProvider>,
 );
