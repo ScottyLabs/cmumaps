3c70a48ac301feb6b91b3549b91825e769d34d87
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-03-28 20:11:55 -0400

    Add path aliases
    Have to set importModuleSpecifier for Typescript in VSCode setting to non-relative to prefer path alias import when quick fix.
    
    https://stackoverflow.com/questions/72029343/typescript-prefers-importing-relative-import-instead-of-path-alias/72029899

---
+++ apps/web/src/components/map-display/MapDisplay.tsx
@@ -0,0 +1,54 @@
+import { Map, MapType, FeatureVisibility } from "mapkit-react";
+
+import { useRef } from "react";
+
+// CMU Campus
+const INITIAL_REGION = {
+  centerLatitude: 40.444,
+  centerLongitude: -79.945,
+  latitudeDelta: 0.006337455593801167,
+  longitudeDelta: 0.011960061265583022,
+};
+
+const CAMERA_BOUNDARY = {
+  centerLatitude: 40.44533940432823,
+  centerLongitude: -79.9457060010195,
+  latitudeDelta: 0.009258427149788417,
+  longitudeDelta: 0.014410141520116326,
+};
+
+const MapDisplay = () => {
+  const mapRef = useRef<mapkit.Map | null>(null);
+
+  return (
+    <Map
+      ref={mapRef}
+      token={import.meta.env.VITE_MAPKIT_TOKEN || ""}
+      initialRegion={INITIAL_REGION}
+      includedPOICategories={[]}
+      cameraBoundary={CAMERA_BOUNDARY}
+      minCameraDistance={5}
+      maxCameraDistance={1500}
+      showsUserLocationControl
+      showsUserLocation={true}
+      mapType={MapType.MutedStandard}
+      // paddingBottom={isMobile ? 72 : 0}
+      paddingBottom={0}
+      paddingLeft={4}
+      paddingRight={4}
+      paddingTop={10}
+      // showsZoomControl={!isMobile}
+      showsCompass={FeatureVisibility.Visible}
+      allowWheelToZoom
+      // onRegionChangeStart={onRegionChangeStart}
+      // onRegionChangeEnd={() => {
+      //   dispatch(setIsZooming(false));
+      //   onRegionChangeEnd();
+      // }}
+      // onClick={handleClick}
+      // onLoad={handleLoad}
+    />
+  );
+};
+
+export default MapDisplay;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@public": "/public",
+    },
+  },
 });
