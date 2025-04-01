5f128923a70ca8fb424d6ddcc09f939dae1a0d69
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-04-01 19:22:01 -0400

    Rename infinite scroll wrapper to events list

--- apps/web/src/components/carnival/events/EventsDisplay.tsx
+++ apps/web/src/components/carnival/events/EventsDisplay.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import InfiniteScrollWrapper from "@/components/carnival/events/displays/InfiniteScrollWrapper";
+import InfiniteScrollWrapper from "@/components/carnival/events/displays/EventsList";
 import EventsDatePicker from "@/components/carnival/events/filters/EventsDatePicker";
 import EventsFilter from "@/components/carnival/events/filters/EventsFilter";
 import {
