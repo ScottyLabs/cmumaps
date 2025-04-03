73be316abc7356f07bef94c0f9d02df06bc13ffc
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-04-03 01:58:01 -0400

    Able to display sprint carnival image

--- apps/web/src/components/info-cards/event-card/EventCard.tsx
+++ apps/web/src/components/info-cards/event-card/EventCard.tsx
@@ -1,9 +1,19 @@
+import InfoCardImage from "@/components/info-cards/shared/media/InfoCardImage";
+
 interface Props {
   eventId: string;
 }
 
 const EventCard = ({ eventId }: Props) => {
-  return <div>{eventId}</div>;
+  return (
+    <div>
+      <InfoCardImage
+        url={"/imgs/carnival/default.png"}
+        alt={"Spring Carnival Image"}
+      />
+      {eventId}
+    </div>
+  );
 };
 
 export default EventCard;
