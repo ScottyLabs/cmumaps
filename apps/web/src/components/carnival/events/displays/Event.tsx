488493ac52784e8a0a45ab02e52711b70ead0dfe
kath <kath45823@gmail.com>
2025-04-02 21:26:10 -0400

    designed event cards

--- apps/web/src/components/carnival/events/displays/Event.tsx
+++ apps/web/src/components/carnival/events/displays/Event.tsx
@@ -5,22 +5,38 @@ interface Props {
 }
 
 const EventDisplay = ({ event }: Props) => {
-  const formatDate = (date: Date) => {
+  const formatTime = (date: Date) => {
     return new Date(date).toLocaleString("en-US", {
-      month: "2-digit",
-      day: "2-digit",
       hour: "2-digit",
       minute: "2-digit",
     });
   };
+
+  const renderDate = (event: EventType) => {
+    if (
+      new Date(event.startTime).getDate() == new Date(event.endTime).getDate()
+    ) {
+      return formatDate(event.startTime);
+    } else {
+      return `${formatDate(event.startTime)} - ${formatDate(event.endTime)}`;
+    }
+  };
+
+  const formatDate = (date: Date) => {
+    return new Date(date).toLocaleString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+    });
+  };
+
   return (
     <div className="flex flex-col">
-      <div className="text-lg font-bold">{event.name}</div>
-      <div className="text-sm text-gray-500">
-        {/* without seconds */}
-        {formatDate(event.startTime)} - {formatDate(event.endTime)}
+      <div className="text-lg font-medium">{event.name}</div>
+      <div className="text-sm text-black">Date: {renderDate(event)}</div>
+      <div className="text-sm text-black">
+        Time: {formatTime(event.startTime)} - {formatTime(event.endTime)}
       </div>
-      <div className="text-sm text-gray-500">{event.location}</div>
+      <div className="text-sm text-black">Location: {event.location}</div>
     </div>
   );
 };
