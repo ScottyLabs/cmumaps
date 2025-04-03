e55c6c6339078bfda3996b66d388a3239187adfd
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-04-02 23:51:23 -0400

    Support database url in server runtime

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 
-export const prisma = new PrismaClient();
+dotenv.config();
+
+export const prisma = new PrismaClient({
+  datasourceUrl:
+    "postgresql://" +
+    process.env.DATABASE_USERNAME +
+    ":" +
+    process.env.DATABASE_PASSWORD +
+    "@" +
+    process.env.DATABASE_HOST +
+    ":" +
+    process.env.DATABASE_PORT +
+    "/" +
+    process.env.DATABASE_NAME,
+});
