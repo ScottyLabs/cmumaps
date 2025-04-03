7f360d9e7381c5da85991e80707b97a29df1282d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-04-03 01:49:35 -0400

    Add spring carnival icons

--- apps/web/src/components/carnival/events/displays/EventBody.tsx
+++ apps/web/src/components/carnival/events/displays/EventBody.tsx
@@ -1,15 +1,22 @@
 import { EventType } from "@cmumaps/common";
 
+import { useNavigate } from "react-router";
+
 interface Props {
   event: EventType;
 }
 
 const EventBody = ({ event }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <div className="mt-2 space-y-2 bg-white p-3 text-sm">
       <div dangerouslySetInnerHTML={{ __html: event.description }} />
       <div className="flex justify-end">
-        <button className="cursor-pointer rounded-lg border-2 bg-blue-500 p-1 text-white">
+        <button
+          className="cursor-pointer rounded-lg border-2 bg-blue-500 p-1 text-white"
+          onClick={() => navigate(`/events/${event.id}`)}
+        >
           View on map
         </button>
       </div>
