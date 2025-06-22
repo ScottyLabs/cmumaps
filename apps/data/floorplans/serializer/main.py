4b5734202e67fe9fe79a8fd6bb9534ce05b6a0d3
ellylai <ellysel@andrew.cmu.edu>
2025-06-22 14:34:43 -0400

    feat: serialize placements.json to new file called placements_serialized.json, added a function in floorService, floorRoutes, and floorController to do so

---
+++ apps/data/floorplans/serializer/main.py
@@ -0,0 +1,22 @@
+# python floorplans/serializer/main.py
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+from auth_utils.get_clerk_jwt import get_clerk_jwt
+
+import requests
+
+import pprint as pprint
+
+# Create buildings.json using Building and Floor table
+    
+# Create all-graph.json using Edge and Node table
+
+# Create floorplans.json using Alias and Room table
+
+# Create placements.json using Floor table
+
+if __name__ == "__main__":
+    pass
