36a54e7e1a9bdbb0177aa42c3f2b5ee01ec8f356
ellylai <ellysel@andrew.cmu.edu>
2025-06-22 16:19:41 -0400

    feat: wrote new endpoints for updating floorplans.json by floor and also creating floorplans_serialized.json

--- apps/server/routes/roomRoutes.ts
+++ apps/server/routes/roomRoutes.ts
@@ -7,5 +7,8 @@ const roomRouter = express.Router();
 roomRouter.post("/:id", roomController.createRoom);
 roomRouter.patch("/:id", roomController.updateRoom);
 roomRouter.delete("/:id", roomController.deleteRoom);
+roomRouter.get("/by-floor/:floorCode", roomController.getRoomIdsByFloor);
+roomRouter.get("/:roomId", roomController.getRoom);
+roomRouter.get("/", roomController.getAllRooms);
 
 export default roomRouter;
