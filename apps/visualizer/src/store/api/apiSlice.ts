c953c607992f19530def870f1507586038e5f64a
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-06-22 15:31:57 -0400

    feat(web&visualizer): use zod to verify environment variables

--- apps/visualizer/src/store/api/apiSlice.ts
+++ apps/visualizer/src/store/api/apiSlice.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import env from "../../env";
 
 export interface BaseMutationArg {
   floorCode: string;
@@ -18,7 +19,7 @@ export const apiSlice = createApi({
   reducerPath: "api",
   tagTypes: ["Graph", "Rooms", "Pois"],
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_SERVER_URL}/api/`,
+    baseUrl: `${env.VITE_SERVER_URL}/api/`,
     prepareHeaders: async (headers) => {
       if (window.Clerk?.session) {
         const token = await getClerkToken();
