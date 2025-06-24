3f3d5783d2e17ae075aadfdba56bd25cfe776495
ellylai <ellysel@andrew.cmu.edu>
2025-06-24 17:56:15 -0400

    feat: serializer main.py to control what files to serialize

--- apps/data/floorplans/serializer/main.py
+++ apps/data/floorplans/serializer/main.py
@@ -1,22 +1,47 @@
-# python floorplans/serializer/main.py
+# python floorplans/serializer/main.py <Optional file_to_serialize> <Optional floor_code>
 import os
 import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-
-from auth_utils.get_clerk_jwt import get_clerk_jwt
-
-import requests
-
-import pprint as pprint
+from placements import placements_serializer
+from buildings import buildings_serializer
+from floorplans import floorplans_serializer
 
 # Create buildings.json using Building and Floor table
-    
 # Create all-graph.json using Edge and Node table
-
 # Create floorplans.json using Alias and Room table
-
 # Create placements.json using Floor table
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) < 2: # serialize all
+        placements_serializer()
+        print("serialized placements.json into placements-serialized.json!")
+        buildings_serializer()
+        print("serialized buildings.json into buildings-serialized.json!")
+        floorplans_serializer()
+        print("serialized floorplans.json into floorplans-serialized.json!")
+        all_graph_serializer()
+        print("serialized all-graph.json into all-graph-serialized.json!")
+    
+    file_to_serialize = str(sys.argv[1])
+    
+    if len(sys.argv) > 2:
+        if file_to_serialize != 'floorplans':
+            print("Usage: python floorplans/serializer/main.py floorplans GH-1")
+            sys.exit(1)
+        # python floorplans/serializer/main.py <floorplans> <GH-1>
+        floor_code = str(sys.argv[2])
+        print(f"Serializing ONLY floor {floor_code}")
+        floorplans_serializer(floor_code)
+    
+    elif file_to_serialize == 'placements':
+        print("serialized placements.json into placements-serialized.json!")
+
+    elif file_to_serialize == 'buildings':
+        print("serialized buildings.json into buildings-serialized.json!")
+    
+    elif file_to_serialize == 'floorplans':
+        floorplans_serializer()
+        print("serialized floorplans.json into floorplans-serialized.json!")
+    
+    elif file_to_serialize == 'all-graph':
+        all_graph_serializer()
+        print("serialized all-graph.json into all-graph-serialized.json!")    
