31bbe3f5e93337e983c92ea4e9aad0074233e2ae
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-01 19:43:16 -0400

    chore(data): add ruff format to package.json

--- apps/data/floorplans/serializer/main.py
+++ apps/data/floorplans/serializer/main.py
@@ -11,7 +11,7 @@ from all_graph import all_graph_serializer
 # Create placements.json using Floor table
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2: # serialize all
+    if len(sys.argv) < 2:  # serialize all
         placements_serializer()
         print("serialized placements.json into placements-serialized.json!")
         buildings_serializer()
@@ -20,28 +20,28 @@ if __name__ == "__main__":
         print("serialized floorplans.json into floorplans-serialized.json!")
         all_graph_serializer()
         print("serialized all-graph.json into all-graph-serialized.json!")
-    
+
     file_to_serialize = str(sys.argv[1])
-    
+
     if len(sys.argv) > 2:
-        if file_to_serialize != 'floorplans':
+        if file_to_serialize != "floorplans":
             print("Usage: python floorplans/serializer/main.py floorplans GH-1")
             sys.exit(1)
         # python floorplans/serializer/main.py <floorplans> <GH-1>
         floor_code = str(sys.argv[2])
         print(f"Serializing ONLY floor {floor_code}")
         floorplans_serializer(floor_code)
-    
-    elif file_to_serialize == 'placements':
+
+    elif file_to_serialize == "placements":
         print("serialized placements.json into placements-serialized.json!")
 
-    elif file_to_serialize == 'buildings':
+    elif file_to_serialize == "buildings":
         print("serialized buildings.json into buildings-serialized.json!")
-    
-    elif file_to_serialize == 'floorplans':
+
+    elif file_to_serialize == "floorplans":
         floorplans_serializer()
         print("serialized floorplans.json into floorplans-serialized.json!")
-    
-    elif file_to_serialize == 'all-graph':
+
+    elif file_to_serialize == "all-graph":
         all_graph_serializer()
         print("serialized all-graph.json into all-graph-serialized.json!")
