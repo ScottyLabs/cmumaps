6fb1063c5ff400d3a27ec8ea41ba388019f18cd5
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-04 19:49:45 -0400

    refactor: display home page with TanStack router

--- apps/visualizer/src/main.tsx
+++ apps/visualizer/src/main.tsx
@@ -1,22 +1,15 @@
-// import {
-//   ClerkLoaded,
-//   ClerkProvider,
-//   RedirectToSignIn,
-//   useUser,
-// } from "@clerk/clerk-react";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-react";
 import type { Clerk } from "@clerk/types";
 import { StrictMode } from "react";
-
-// import { createRoot } from "react-dom/client";
-// import { Provider } from "react-redux";
-// import { BrowserRouter, Outlet, Route, Routes } from "react-router";
-
-// import "./index.css";
-// import env from "./env";
-// import FloorPage from "./pages/FloorPage";
-// import Home from "./pages/Home";
-// import { USE_STRICT_MODE } from "./settings";
-// import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import ReactDOM from "react-dom/client";
+import env from "./env";
+import { routeTree } from "./routeTree.gen";
+import { USE_STRICT_MODE } from "./settings";
+import { store } from "./store/store";
 
 // https://clerk.com/docs/components/control/clerk-loaded
 declare global {
@@ -25,53 +18,10 @@ declare global {
   }
 }
 
-// const PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY;
-// if (!PUBLISHABLE_KEY) {
-//   throw new Error("Add your Clerk Publishable Key to the .env file");
-// }
-
-// const ProtectedRoute = () => {
-//   const { isSignedIn } = useUser();
-
-//   if (!isSignedIn) {
-//     return <RedirectToSignIn />;
-//   }
-
-//   return <Outlet />;
-// };
-
-// const AppContent = () => (
-//   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-//     <ClerkLoaded>
-//       <BrowserRouter>
-//         <Provider store={store}>
-//           <Routes>
-//             <Route element={<ProtectedRoute />}>
-//               <Route index element={<Home />} />
-//               <Route path=":floorCode" element={<FloorPage />} />
-//             </Route>
-//           </Routes>
-//         </Provider>
-//       </BrowserRouter>
-//     </ClerkLoaded>
-//   </ClerkProvider>
-// );
-
-// createRoot(document.getElementById("root") as HTMLElement).render(
-//   USE_STRICT_MODE ? (
-//     <StrictMode>
-//       <AppContent />
-//     </StrictMode>
-//   ) : (
-//     <AppContent />
-//   ),
-// );
-
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-import ReactDOM from "react-dom/client";
-
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+const PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -83,13 +33,29 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const AppContent = () => (
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkLoaded>
+      <BrowserRouter>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </BrowserRouter>
+    </ClerkLoaded>
+  </ClerkProvider>
+);
+
 // Render the app
 const rootElement = document.getElementById("root") as HTMLElement;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
+    USE_STRICT_MODE ? (
+      <StrictMode>
+        <AppContent />
+      </StrictMode>
+    ) : (
+      <AppContent />
+    ),
   );
 }
--- apps/visualizer/src/routes/index.tsx
+++ apps/visualizer/src/routes/index.tsx
@@ -1,13 +1,62 @@
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 import { createFileRoute } from "@tanstack/react-router";
+import { NavLink } from "react-router";
+import ErrorDisplay from "../components/shared/ErrorDisplay";
+import Loader from "../components/shared/Loader";
+import MyToastContainer from "../components/shared/MyToastContainer";
+import useWebSocket from "../hooks/useWebSocket";
+import { useGetBuildingCodesAndNamesQuery } from "../store/api/buildingApiSlice";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  useWebSocket();
+
+  const {
+    data: buildingCodesAndNames,
+    isLoading,
+    isError,
+  } = useGetBuildingCodesAndNamesQuery();
+
+  if (isLoading) {
+    return <Loader loadingText="Fetching building codes" />;
+  }
+
+  if (isError || !buildingCodesAndNames) {
+    return <ErrorDisplay errorText="Failed to fetch building codes" />;
+  }
+
+  const renderTopBar = () => (
+    <header className="m-2 flex flex-row-reverse">
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+
+  const renderBuildingLinks = () => (
+    <div className="m-5 flex flex-wrap gap-8">
+      {buildingCodesAndNames.map(({ buildingCode, name }) => (
+        <NavLink
+          to={`/${buildingCode}`}
+          key={buildingCode}
+          className="cursor-pointer rounded-lg border border-gray-300 p-4 shadow-md transition duration-200 ease-in-out hover:scale-105 hover:shadow-lg"
+        >
+          {name}
+        </NavLink>
+      ))}
+    </div>
+  );
+
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div>
+      {renderTopBar()}
+      {renderBuildingLinks()}
+      <MyToastContainer />
     </div>
   );
 }
+
+export default Index;
