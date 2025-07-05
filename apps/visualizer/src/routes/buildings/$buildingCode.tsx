0cc174e63f4a8a3d02129e083b2c50ca7b418333
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-04 21:39:47 -0400

    feat(visualizer): perfect the error handling logic

--- apps/visualizer/src/routes/buildings/$buildingCode.tsx
+++ apps/visualizer/src/routes/buildings/$buildingCode.tsx
@@ -1,5 +1,6 @@
-import { ERROR_CODES } from "@cmumaps/common";
+import type { ErrorCode } from "@cmumaps/common";
 import { createFileRoute, Navigate } from "@tanstack/react-router";
+import ErrorHandler from "../../components/shared/ErrorHandler";
 import Loader from "../../components/shared/Loader";
 import { useGetDefaultFloorQuery } from "../../store/api/buildingApiSlice";
 
@@ -9,21 +10,18 @@ export const Route = createFileRoute("/buildings/$buildingCode")({
 
 function RouteComponent() {
   const { buildingCode } = Route.useParams();
-
-  // Skip the query if don't need to retrieve the default floor
-  const { data, isLoading, isError } = useGetDefaultFloorQuery(buildingCode);
+  const { data, isLoading, error } = useGetDefaultFloorQuery(buildingCode);
 
   if (isLoading) {
     return <Loader loadingText="Fetching building floors" />;
   }
 
-  if (isError) {
-    return (
-      <Navigate
-        to="/"
-        params={{ errorCode: ERROR_CODES.INVALID_BUILDING_CODE }}
-      />
-    );
+  if (error) {
+    if ("data" in error && "code" in (error.data as object)) {
+      const errorCode = (error.data as { code: ErrorCode }).code;
+      return <Navigate to="/" search={{ errorCode }} />;
+    }
+    return <ErrorHandler error={error} />;
   }
 
   if (!data) {
