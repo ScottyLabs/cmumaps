f6cf3a326cef34be5ed259548169d0cdd300d3ba
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-08 22:28:35 -0400

    revert: revert "chore: remove cors"
    This reverts commit 3f9b612d74c9ef1642e20aad315a52087a523353.

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -13,7 +13,10 @@ import { WebSocketService } from "./src/services/webSocketService";
 
 const app = express();
 const corsOptions = {
-  origin: "*",
+  origin: process.env.ALLOWED_ORIGINS_REGEX?.split(",").map(
+    (origin) => new RegExp(origin),
+  ),
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "8mb" }));
