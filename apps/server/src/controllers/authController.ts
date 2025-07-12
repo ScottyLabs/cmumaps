6e1d591286f108bdc76d232741ac0a2cd309338a
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-12 18:58:26 -0400

    refactor: use await instead of then in  authController.ts

--- apps/server/src/controllers/authController.ts
+++ apps/server/src/controllers/authController.ts
@@ -16,24 +16,19 @@ export class AuthController {
   public async userInfo(
     @Request() request: express.Request,
   ): Promise<UserInfoResponse> {
-    if (!request.user) {
-      return { user: null };
-    }
+    if (!request.user) return { user: null };
 
-    return fetch("https://auth.slabs-dev.org/application/o/userinfo/", {
-      headers: {
-        Authorization: `Bearer ${request.user?.token}`,
+    const url = "https://auth.slabs-dev.org/application/o/userinfo/";
+    const res = await fetch(url, {
+      headers: { Authorization: `Bearer ${request.user?.token}` },
+    });
+    const data = await res.json();
+    return {
+      user: {
+        id: data.sub as string,
+        email: data.email as string,
+        name: data.name as string,
       },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        return {
-          user: {
-            id: data.sub as string,
-            email: data.email as string,
-            name: data.name as string,
-          },
-        };
-      });
+    };
   }
 }
