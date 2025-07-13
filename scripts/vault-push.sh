2f97726db367926158602e45018f47eeb082ebca
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-13 13:46:56 -0400

    ci: put all environment variables in Hashicorp Vault

--- scripts/vault-push.sh
+++ scripts/vault-push.sh
@@ -1,5 +1,25 @@
-export VAULT_ADDR=https://secrets.scottylabs.org 
-APPLICATIONS=("web" "visualizer" "server" "rust-server" "data")
-for APP in "${APPLICATIONS[@]}"; do
-    cat apps/$APP/.env | xargs -r vault kv put -mount="ScottyLabs" "cmumaps/dev/$APP"
+ENVIRONMENT="$1"
+if [ "$ENVIRONMENT" == "all" ]; then
+  ENVIRONMENT=("local" "dev" "staging" "prod")
+else
+  ENVIRONMENT=("$ENVIRONMENT")
+fi
+
+APPLICATION="$2"
+if [ "$APPLICATION" == "all" ]; then
+  APPLICATIONS=("web" "visualizer" "server" "rust-server" "data")
+else
+  APPLICATIONS=("$APPLICATION")
+fi
+
+export VAULT_ADDR=https://secrets.scottylabs.org
+for ENV in "${ENVIRONMENT[@]}"; do
+  ENV_FILE_SUFFIX=""
+  if [ "$ENV" != "local" ]; then
+    ENV_FILE_SUFFIX=".$ENV"
+  fi
+
+  for APP in "${APPLICATIONS[@]}"; do
+    cat apps/$APP/.env$ENV_FILE_SUFFIX | xargs -r vault kv put -mount="ScottyLabs" "cmumaps/$ENV/$APP"
+  done
 done
