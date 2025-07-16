92bd1ba0b3ddbebbc5998d2d10d7a84cf6886dda
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-16 00:22:14 -0400

    chore(web): update zod to use url()

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 // Define the schema as an object with all of the env
 // variables and their types
 const envSchema = z.object({
-  VITE_SERVER_URL: z.string().url(),
-  VITE_RUST_SERVER_URL: z.string().url(),
-  VITE_LOGIN_URL: z.string().url(),
+  VITE_LOGIN_URL: z.url(),
   VITE_MAPKIT_TOKEN: z.string().optional(),
   VITE_PUBLIC_POSTHOG_HOST: z.string().optional(),
   VITE_PUBLIC_POSTHOG_KEY: z.string().optional(),
+  VITE_RUST_SERVER_URL: z.url(),
+  VITE_SERVER_URL: z.url(),
 });
 
 // Validate `process.env` against our schema
