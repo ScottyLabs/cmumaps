72791d3e4fe038ec07da47aa3c4000adfa427593
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-07-21 21:48:07 -0400

    fix: add auth to database population routes with scope

--- apps/server/src/routes/dropTablesRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { dropTablesController } from "../controllers/dropTablesController";
-
-const dropTablesRouter = express.Router();
-
-dropTablesRouter.delete("/", dropTablesController.dropTables);
-
-export default dropTablesRouter;
