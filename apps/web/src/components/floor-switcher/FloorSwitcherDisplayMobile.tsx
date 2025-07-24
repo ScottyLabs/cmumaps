f61c7d20c345c717b7a9ac284b2a38e59009f71c
luke992 <luke.hann@hannmfg.com>
2025-07-24 17:31:00 -0400

    fix(web): prevent default on drag event on floor switcher, double size of draggable region

--- apps/web/src/components/floor-switcher/FloorSwitcherDisplayMobile.tsx
+++ apps/web/src/components/floor-switcher/FloorSwitcherDisplayMobile.tsx
@@ -1,6 +1,6 @@
 import type { Building } from "@cmumaps/common";
 import { animate, motion, useMotionValue } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useBoundStore from "@/store";
 import FloorSwitcherCarouselMobile from "./FloorSwitcherCarouselMobile";
 
@@ -12,6 +12,8 @@ interface Props {
 const FloorSwitcherDisplayMobile = ({ building, initialFloorLevel }: Props) => {
   const focusFloor = useBoundStore((state) => state.focusFloor);
   const floorIndex = building.floors.indexOf(initialFloorLevel);
+
+  const draggableRegionRef = useRef<HTMLDivElement>(null);
 
   const progressValue = useMotionValue(floorIndex);
 
@@ -61,25 +63,37 @@ const FloorSwitcherDisplayMobile = ({ building, initialFloorLevel }: Props) => {
     return controls.stop;
   };
 
+  //e.preventDefault can only be called in an active event listener
+  useEffect(() => {
+    const el = draggableRegionRef.current;
+    if (!el) return;
+    const handler = (e: TouchEvent) => {
+      e.preventDefault();
+    };
+    el.addEventListener("touchmove", handler, { passive: false });
+    return () => el.removeEventListener("touchmove", handler);
+  }, []);
+
   return (
-    <motion.div
-      className="btn-shadow -translate-x-1/2 -translate-y-1/2 fixed top-1/2 h-52 w-45 bg-white/10 shadow-black/20 shadow-lg backdrop-blur-md"
-      style={{ borderRadius: "50% / 50%" }}
-      onTouchStart={(e) => {
-        onTouchStart(e);
-      }}
-      onTouchMove={(e) => {
-        onTouchMove(e);
-      }}
-      onTouchEnd={() => {
-        onTouchEnd();
-      }}
-    >
-      <FloorSwitcherCarouselMobile
-        building={building}
-        progressValue={progressValue}
+    <>
+      <div
+        className="-translate-x-1/2 -translate-y-1/2 fixed top-1/2 h-104 w-90"
+        style={{ borderRadius: "50% / 50%" }}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+        ref={draggableRegionRef}
       />
-    </motion.div>
+      <motion.div
+        className="btn-shadow -translate-x-1/2 -translate-y-1/2 fixed top-1/2 h-52 w-45 bg-white/10 backdrop-blur-md"
+        style={{ borderRadius: "50% / 50%" }}
+      >
+        <FloorSwitcherCarouselMobile
+          building={building}
+          progressValue={progressValue}
+        />
+      </motion.div>
+    </>
   );
 };
 
