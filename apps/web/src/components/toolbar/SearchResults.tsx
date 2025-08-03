0f8288730c0fc01b26efb8b6f50be50642048d74
luke992 <luke.hann@hannmfg.com>
2025-08-03 11:47:56 -0400

    feat(web): zoom in on location when search result clicked

--- apps/web/src/components/toolbar/SearchResults.tsx
+++ apps/web/src/components/toolbar/SearchResults.tsx
@@ -1,18 +1,23 @@
 import { useMemo } from "react";
 import { useNavigate } from "react-router";
+import $api from "@/api/client";
 import $rapi from "@/api/rustClient";
 import classroomIcon from "@/assets/icons/search_results/study.svg";
 import useBoundStore from "@/store";
+import { getFloorLevelFromRoomName } from "@/utils/floorUtils";
+import prefersReducedMotion from "@/utils/prefersReducedMotion";
+import { zoomOnObject } from "@/utils/zoomUtils";
 
 interface Props {
   mapRef: React.RefObject<mapkit.Map | null>;
   searchQuery: string;
 }
 
-const SearchResults = ({ searchQuery }: Props) => {
+const SearchResults = ({ searchQuery, mapRef }: Props) => {
   // Global state
   const isSearchOpen = useBoundStore((state) => state.isSearchOpen);
   const hidesSearch = useBoundStore((state) => state.hideSearch);
+  const focusFloor = useBoundStore((state) => state.focusFloor);
 
   const navigate = useNavigate();
 
@@ -20,6 +25,8 @@ const SearchResults = ({ searchQuery }: Props) => {
     params: { query: { query: searchQuery } },
     enabled: isSearchOpen && searchQuery.length > 0,
   });
+
+  const { data: buildings } = $api.useQuery("get", "/buildings");
 
   const organizedResults = useMemo(() => {
     if (!searchResults) return [];
@@ -47,9 +54,49 @@ const SearchResults = ({ searchQuery }: Props) => {
           className="flex h-19 w-full items-center bg-white text-lg active:bg-[#dce8f6]"
           onClick={() => {
             if (result.type === "room") {
-              navigate(`/${result.nameWithSpace?.split(" ").join("-")}`);
+              const roomName = result.nameWithSpace?.split(" ")[1];
+              const buildingName = result.nameWithSpace?.split(" ")[0];
+              const floor = getFloorLevelFromRoomName(roomName);
+              console.log(`Room: ${roomName}`);
+              console.log(`Floor: ${floor}`);
+              if (buildingName && floor) {
+                focusFloor({ buildingCode: buildingName, level: floor });
+                navigate(`/${buildingName}-${roomName}`);
+              }
+              const latitude = result.labelPosition?.latitude;
+              const longitude = result.labelPosition?.longitude;
+              if (latitude && longitude && mapRef.current) {
+                // zoomOnPoint(
+                //   mapRef.current,
+                //   new mapkit.Coordinate(latitude, longitude),
+                // );
+                // if (floor) {
+                //   zoomOnFloor(mapRef.current, buildings, floor);
+                // }
+                // if (buildingName && buildings?.[buildingName]) {
+                //   zoomOnObject(
+                //     mapRef.current,
+                //     buildings[buildingName].shape.flat(),
+                //   );
+                // } else {
+                const offset = 0.0005;
+                mapRef.current.setRegionAnimated(
+                  new mapkit.BoundingRegion(
+                    latitude + offset,
+                    longitude + offset,
+                    latitude - offset,
+                    longitude - offset,
+                  ).toCoordinateRegion(),
+                  !prefersReducedMotion(),
+                );
+                // }
+              }
             } else {
-              navigate(`/${result.id}`);
+              navigate(`/ ${result.id} `);
+              const building = buildings?.[result.id];
+              if (building && mapRef.current) {
+                zoomOnObject(mapRef.current, building.shape.flat());
+              }
             }
             hidesSearch();
           }}
@@ -60,7 +107,7 @@ const SearchResults = ({ searchQuery }: Props) => {
                 {result.id}
               </div>
               <div className="flex flex-col">
-                {/* {result.alias && result.type === "room" && `${result.alias} - `} */}
+                {/* {result.alias && result.type === "room" && `${ result.alias } - `} */}
                 {result.fullNameWithSpace}
               </div>
             </>
@@ -71,8 +118,10 @@ const SearchResults = ({ searchQuery }: Props) => {
               </div>
               <div className="flex w-[100%] justify-between">
                 <div className="flex flex-col">
-                  {/* {result.alias && result.type === "room" && `${result.alias} - `} */}
-                  {result.nameWithSpace}
+                  {/* {result.alias && result.type === "room" && `${ result.alias } - `} */}
+                  {result.alias && result.alias !== ""
+                    ? result.alias
+                    : result.fullNameWithSpace}
                 </div>
                 <div className="flex flex-col self-end pr-5 text-[#9ca3af]">
                   Rooms
@@ -84,8 +133,6 @@ const SearchResults = ({ searchQuery }: Props) => {
       ))}
     </div>
   );
-
-  // TODO: Render results
 };
 
 export default SearchResults;
