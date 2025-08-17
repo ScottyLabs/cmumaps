cd85980be87cfdf30bea06a39e9107a0d0622d25
luke992 <luke.hann@hannmfg.com>
2025-08-16 22:21:27 -0400

    feat(web): improve the placeholders for the 'About CMU Maps' and 'Schedule' menus

--- apps/web/src/components/ui-layout/icons-display/mobile/AboutMenu.tsx
+++ apps/web/src/components/ui-layout/icons-display/mobile/AboutMenu.tsx
@@ -5,10 +5,22 @@ interface Props {
 const AboutMenu = ({ lowerHeight }: Props) => {
   return (
     <div
-      className="btn-shadow-dark fixed inset-x-5 top-5 z-50 overflow-auto rounded-lg bg-white shadow-lg"
+      className="btn-shadow-dark fixed inset-x-5 z-50 overflow-auto rounded-lg bg-white shadow-lg"
       style={{ bottom: lowerHeight }}
     >
-      <h5 className="px-2 py-1">About CMU Maps</h5>
+      <div className="w-full py-2 text-center font-bold text-xl">
+        About CMU Maps
+      </div>
+      <div className="flex w-full justify-center py-2 text-lg">
+        <a
+          href="https://docs.google.com/document/d/1jZeIij72ovf3K2J1J57rlD4pz3xnca3BfPedg9Ff1sc/edit?tab=t.0#heading=h.ec0fj4jqvpj"
+          className="text-primary-blue underline"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Feedback Form
+        </a>
+      </div>
     </div>
   );
 };
