b6ddacad0217ac64365549c5d95f537b9546a6b5
luke992 <luke.hann@hannmfg.com>
2025-08-16 22:21:27 -0400

    feat(web): improve the placeholders for the 'About CMU Maps' and 'Schedule' menus

--- apps/web/src/components/ui-layout/icons-display/mobile/CoursesMenu.tsx
+++ apps/web/src/components/ui-layout/icons-display/mobile/CoursesMenu.tsx
@@ -5,10 +5,22 @@ interface Props {
 const CoursesMenu = ({ lowerHeight }: Props) => {
   return (
     <div
-      className="btn-shadow-dark fixed inset-x-5 top-5 z-50 overflow-auto rounded-lg bg-white shadow-lg"
+      className="btn-shadow-dark fixed inset-x-5 z-50 overflow-auto rounded-lg bg-white shadow-lg"
       style={{ bottom: lowerHeight }}
     >
-      <h5 className="px-2 py-1">Schedule</h5>
+      <div className="w-full py-2 text-center font-bold text-xl">Schedule</div>
+      <div className="w-full justify-center px-2 py-2 text-lg">
+        Integration with course schedules from{" "}
+        <a
+          href="https://cmucourses.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-primary-blue underline"
+        >
+          CMU Courses
+        </a>{" "}
+        is in development.
+      </div>
     </div>
   );
 };
