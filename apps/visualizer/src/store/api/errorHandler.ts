2df85aed5035eb8f887db919456007b36574c126
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-12 16:20:19 -0400

    feat: specific error toast for authorized edit

--- apps/visualizer/src/store/api/errorHandler.ts
+++ apps/visualizer/src/store/api/errorHandler.ts
@@ -8,7 +8,11 @@ export const handleQueryError = async (
   try {
     await queryFulfilled;
   } catch (e) {
-    toast.error("Failed to save! Check the Console for detailed error.");
+    if ((e as { error: { status: number } }).error.status === 404) {
+      toast.error("You are not authorized to edit!");
+    } else {
+      toast.error("Failed to save! Check the Console for detailed error.");
+    }
     undo();
     console.error(e);
   }
