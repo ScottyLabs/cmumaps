f2a6a3b3e15af8cd3c3f7577315a5ed012f3016e
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-15 17:04:04 -0400

    feat(visualizer): add navbar and view switch to map view

--- apps/visualizer/src/routes/map/$floorCode.tsx
+++ apps/visualizer/src/routes/map/$floorCode.tsx
@@ -3,9 +3,11 @@ import { CAMERA_BOUNDARY, INITIAL_REGION } from "@cmumaps/ui";
 import { createFileRoute } from "@tanstack/react-router";
 import { FeatureVisibility, Map as MapkitMap, MapType } from "mapkit-react";
 import { useEffect, useState } from "react";
+import NavBar from "@/components/ui-layout/NavBar";
 import BuildingShape from "../../components/map-view/BuildingShape";
 import FloorplanOverlay from "../../components/map-view/FloorplanOverlay";
 import PlacementPanel from "../../components/map-view/PlacementPanel";
+import ViewSwitch from "../../components/ui-layout/ViewSwitch";
 import env from "../../env";
 import { useGetBuildingQuery } from "../../store/api/buildingApiSlice";
 import { useGetFloorPlacementQuery } from "../../store/api/floorDataApiSlice";
@@ -32,29 +34,33 @@ function MapView() {
   }
 
   return (
-    <main className="relative h-dvh">
-      <MapkitMap
-        token={env.VITE_MAPKIT_TOKEN || ""}
-        initialRegion={INITIAL_REGION}
-        includedPOICategories={[]}
-        cameraBoundary={CAMERA_BOUNDARY}
-        minCameraDistance={5}
-        maxCameraDistance={1500}
-        showsUserLocationControl
-        showsUserLocation={true}
-        mapType={MapType.MutedStandard}
-        paddingBottom={0}
-        paddingLeft={4}
-        paddingRight={4}
-        paddingTop={10}
-        showsZoomControl={true}
-        showsCompass={FeatureVisibility.Visible}
-        allowWheelToZoom
-      >
-        <PlacementPanel placement={placement} setPlacement={setPlacement} />
-        <BuildingShape building={building} />
-        <FloorplanOverlay floorCode={floorCode} placement={placement} />
-      </MapkitMap>
-    </main>
+    <>
+      <NavBar floorCode={floorCode} />
+      <ViewSwitch floorCode={floorCode} />
+      <div className="relative h-dvh">
+        <MapkitMap
+          token={env.VITE_MAPKIT_TOKEN || ""}
+          initialRegion={INITIAL_REGION}
+          includedPOICategories={[]}
+          cameraBoundary={CAMERA_BOUNDARY}
+          minCameraDistance={5}
+          maxCameraDistance={1500}
+          showsUserLocationControl
+          showsUserLocation={true}
+          mapType={MapType.MutedStandard}
+          paddingBottom={0}
+          paddingLeft={4}
+          paddingRight={4}
+          paddingTop={10}
+          showsZoomControl={true}
+          showsCompass={FeatureVisibility.Visible}
+          allowWheelToZoom
+        >
+          <PlacementPanel placement={placement} setPlacement={setPlacement} />
+          <BuildingShape building={building} />
+          <FloorplanOverlay floorCode={floorCode} placement={placement} />
+        </MapkitMap>
+      </div>
+    </>
   );
 }
