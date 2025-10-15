e9d750456538b8af5290dd425f0088990ecc3207
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-15 18:21:52 -0400

    feat(visualizer): finalize UI for placement

--- apps/visualizer/src/components/map-view/PlacementPanel.tsx
+++ apps/visualizer/src/components/map-view/PlacementPanel.tsx
@@ -15,7 +15,17 @@ const PlacementPanel = ({ placement, setPlacement }: Props) => {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
   ) => {
     return (
-      <div className="w-48">
+      <div className="flex w-full items-center gap-2">
+        {label}:{" "}
+        <input
+          className="w-fit cursor-pointer rounded-lg bg-blue-200 px-1"
+          type="number"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={onChange}
+        />
         <input
           type="range"
           min={min}
@@ -23,20 +33,8 @@ const PlacementPanel = ({ placement, setPlacement }: Props) => {
           step={step}
           value={value}
           onChange={onChange}
-          className="h-2 w-full cursor-pointer rounded-lg bg-blue-400"
+          className="h-2 w-full cursor-pointer justify-center rounded-lg"
         />
-        <div className="text-center text-lg">
-          {label}:{" "}
-          <input
-            className="w-fit"
-            type="number"
-            min={min}
-            max={max}
-            step={step}
-            value={value}
-            onChange={onChange}
-          />
-        </div>
       </div>
     );
   };
@@ -77,38 +75,64 @@ const PlacementPanel = ({ placement, setPlacement }: Props) => {
       });
     };
 
+    const renderInput = (
+      label: string,
+      value: number,
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    ) => {
+      return (
+        <div>
+          {label}:{" "}
+          <input
+            type="number"
+            step={0.000001}
+            value={value}
+            onChange={onChange}
+            className="cursor-pointer rounded-lg bg-blue-200 px-1"
+          />
+        </div>
+      );
+    };
+
     return (
-      <div className="mt-3 text-left text-lg">
-        <div>
-          Latitude:{" "}
-          <input
-            type="number"
-            step={0.000001}
-            value={placement.geoCenter.latitude}
-            onChange={handleLatitudeChange}
-          />
-        </div>
-        <div>
-          Longitude:{" "}
-          <input
-            type="number"
-            step={0.000001}
-            value={placement.geoCenter.longitude}
-            onChange={handleLongitudeChange}
-          />
-        </div>
-      </div>
+      <>
+        {renderInput(
+          "Latitude",
+          placement.geoCenter.latitude,
+          handleLatitudeChange,
+        )}
+        {renderInput(
+          "Longitude",
+          placement.geoCenter.longitude,
+          handleLongitudeChange,
+        )}
+      </>
+    );
+  };
+
+  const renderConfirmButton = () => {
+    return (
+      <button
+        type="button"
+        className="cursor-pointer rounded-lg bg-blue-500 px-4 py-2 text-base text-white"
+        onClick={() => {
+          console.log("Confirm Placement");
+        }}
+      >
+        Confirm Placement
+      </button>
     );
   };
 
   return (
     <div className="-translate-y-1/2 fixed top-1/2 z-50">
-      <div className="h-64 w-fit rounded-lg border bg-slate-400 shadow-lg">
+      <div className="h-66 w-fit rounded-lg border bg-slate-400 shadow-lg">
         <h1 className="pt-2 text-center text-xl underline">Placement</h1>
-        <div className="mt-2 flex flex-col gap-2 px-2">
+        <div className="mt-3 flex flex-col gap-3 px-2 text-lg">
           {renderScaleControl()}
           {renderAngleControl()}
           {renderCenterControl()}
+          {renderConfirmButton()}
         </div>
       </div>
     </div>
