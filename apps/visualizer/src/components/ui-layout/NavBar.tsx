07724bc34f414ba377e60dcb376fbf8335abcefe
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-10-15 14:32:49 -0400

    fix(visualizer): have building name align in navbar

--- apps/visualizer/src/components/ui-layout/NavBar.tsx
+++ apps/visualizer/src/components/ui-layout/NavBar.tsx
@@ -16,7 +16,7 @@ const NavBar = ({ floorCode }: Props) => {
         <div className="font-bold text-white text-xl">
           CMU Maps Data Visualization
         </div>
-        <div className="-translate-x-1/2 fixed top-4 left-1/2 h-7 text-white text-xl">
+        <div className="-translate-x-1/2 absolute top-4 left-1/2 h-7 text-white text-xl">
           {name ? name : ""}
         </div>
         <Link
