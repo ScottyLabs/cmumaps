ef7273f8d157ae4cf8bcad51ef0b98b087be5d9d
luke992 <luke.hann@hannmfg.com>
2025-11-01 06:36:14 -0400

    feat(web): Add Nova banner

---
+++ apps/web/src/components/ui-layout/Banner.tsx
@@ -0,0 +1,24 @@
+const Banner = () => {
+  const link = (
+    <a
+      className="underline"
+      href="https://docs.google.com/forms/d/e/1FAIpQLSd6mXSOzxxUctc0EeQBTanqebc31xmBnKb_cFRosqHjtmuemg/viewform"
+    >
+      Register
+    </a>
+  );
+  (" ");
+
+  return (
+    <div className="bg-primary-blue px-4 py-2 text-center text-white">
+      <h1 className="font-bold text-xl">
+        <div className="hidden sm:block">
+          {link} for NOVA, Scottylabs' GenAI Hackathon by Nov. 1st!
+        </div>
+        <div className="block sm:hidden">{link} for NOVA by Nov. 1st!</div>
+      </h1>
+    </div>
+  );
+};
+
+export default Banner;
