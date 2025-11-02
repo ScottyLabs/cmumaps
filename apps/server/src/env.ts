f5cd9789082ad25bc4b347fd4ac1d0aabe04e681
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-11-01 21:19:28 -0400

    chore(server): remove NODE_ENV env var

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 // Define the schema as an object with all of the env
 // variables and their types
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]),
   SERVER_PORT: z.number().default(80),
   CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
