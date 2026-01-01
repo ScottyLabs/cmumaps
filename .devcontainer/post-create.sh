b0ad0816c38b6f1431ea1ea08ea2f25b0b61c3f1
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-12-31 20:40:22 -0500

    chore: manually install editorconfig instead of using homebrew

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,41 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+set -e
+
+# Download the editorconfig-checker binary manually
+# Couldn't use the editorconfig-checker npm package because it fails to download
+# the binary when running in the dev container.
+install_editorconfig_checker() {
+  # Fetch latest version tag from GitHub API
+  local latest_version=$(curl -s https://api.github.com/repos/editorconfig-checker/editorconfig-checker/releases/latest | jq -r '.tag_name')
+
+  # Get OS from uname by converting to lowercase
+  local os=$(uname | tr '[:upper:]' '[:lower:]')
+
+  # `uname -m` returns aarch64 instead of arm64 in dev container
+  # so we use Bun to get the correct architecture
+  local arch=$(bun --eval "import os from 'os'; console.log(os.arch())")
+
+  # Create destination directory for the binary
+  local dest_dir="ec"
+  mkdir -p "$dest_dir"
+
+  # Download the binary https://github.com/editorconfig-checker/editorconfig-checker?tab=readme-ov-file#quickstart
+  # Move the binary to the destination directory and extract it
+  local file_name="ec-$os-$arch.tar.gz"
+  curl -O -L -C - https://github.com/editorconfig-checker/editorconfig-checker/releases/download/$latest_version/$file_name &&
+    mv "$file_name" "$dest_dir/$file_name" &&
+    tar xzf "$dest_dir/$file_name" -C "$dest_dir"
+
+  # Create alias for editorconfig-checker
+  echo "alias editorconfig-checker='$PWD/$dest_dir/bin/ec-$os-$arch'" >>~/.zshrc
+}
+
+install_editorconfig_checker
+exit 0
 
 # Activate Bun completions in zsh on startup
 if ! grep -q 'source <(SHELL=zsh bun completions)' ~/.zshrc; then
-    echo 'source <(SHELL=zsh bun completions)' >>~/.zshrc
+  echo 'source <(SHELL=zsh bun completions)' >>~/.zshrc
 fi
 
 # Install Bun dependencies
@@ -30,7 +63,7 @@ pip install -r requirements.txt
 
 # Activate the virtual environment in zsh on startup
 if ! grep -q "source .venv/bin/activate" ~/.zshrc; then
-    echo "source .venv/bin/activate" >>~/.zshrc
+  echo "source .venv/bin/activate" >>~/.zshrc
 fi
 
 # Populate the database
