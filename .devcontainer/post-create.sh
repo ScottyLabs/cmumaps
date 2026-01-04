07ff08373d01bd2202796fcca4ecc5eb4636a84f
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-04 16:29:28 -0500

    chore: install dependencies more reliably with homebrew

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,60 @@
 #!/usr/bin/env bash
 set -e
 
+# Install Bun for JavaScript package management: https://bun.com/get
+# We have to install npm to install bun due to:
+# Download failed: https://github.com/oven-sh/bun/releases/download/bun-v1.3.5/bun-linux-aarch64.zip
+# curl: (35) LibreSSL SSL_connect: SSL_ERROR_ZERO_RETURN in connection to release-assets.githubusercontent.com:443
+brew install npm
+npm install -g bun
+
+# Activate Bun completions in zsh on startup
+if ! grep -q 'source <(SHELL=zsh bun completions)' ~/.zshrc; then
+  echo 'source <(SHELL=zsh bun completions)' >>~/.zshrc
+fi
+
+# Install Bun dependencies
+bun install
+
+# Set up environment variables
+bun run secrets:setup
+bun run secrets:pull all all
+
+# Push the database schema and generate OpenAPI spec
+cd apps/server
+bunx prisma db push
+bun run tsoa
+bun run openapi
+
+# Start the server in the background (without console output) for data population
+bun run dev >/dev/null 2>&1 &
+
+# Install UV for Python package management:
+# https://docs.astral.sh/uv/getting-started/installation/#homebrew
+brew install uv
+
+# Create a virtual environment for Python
+cd ../..
+python3 -m venv .venv
+source .venv/bin/activate
+cd apps/data
+pip install -r requirements.txt
+
+# Activate the virtual environment in zsh on startup
+if ! grep -q "source .venv/bin/activate" ~/.zshrc; then
+  echo "source .venv/bin/activate" >>~/.zshrc
+fi
+
+# Populate the database
+python3 floorplans/deserializer/database_population.py
+
+# Kill the server
+kill %1
+
+# Install shfmt for shell script formatting
+# https://formulae.brew.sh/formula/shfmt
+brew install shfmt
+
 # Download the editorconfig-checker binary manually
 # Couldn't use the editorconfig-checker npm package because it fails to download
 # the binary when running in the dev container.
@@ -31,42 +85,3 @@ install_editorconfig_checker() {
 }
 
 install_editorconfig_checker
-
-# Activate Bun completions in zsh on startup
-if ! grep -q 'source <(SHELL=zsh bun completions)' ~/.zshrc; then
-  echo 'source <(SHELL=zsh bun completions)' >>~/.zshrc
-fi
-
-# Install Bun dependencies
-bun install
-
-# Set up environment variables
-bun run secrets:setup
-bun run secrets:pull all all
-
-# Push the database schema and generate OpenAPI spec
-cd apps/server
-bunx prisma db push
-bun run tsoa
-bun run openapi
-
-# Start the server in the background (without console output) for data population
-bun run dev >/dev/null 2>&1 &
-
-# Create a virtual environment for Python
-cd ../..
-python3 -m venv .venv
-source .venv/bin/activate
-cd apps/data
-pip install -r requirements.txt
-
-# Activate the virtual environment in zsh on startup
-if ! grep -q "source .venv/bin/activate" ~/.zshrc; then
-  echo "source .venv/bin/activate" >>~/.zshrc
-fi
-
-# Populate the database
-python3 floorplans/deserializer/database_population.py
-
-# Kill the server
-kill %1
