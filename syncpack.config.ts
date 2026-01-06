7969f5f5ccbd0cbc1394d5976d918d46f4294a57
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-06 11:16:18 -0500

    chore: pin the Biome version

--- syncpack.config.ts
+++ syncpack.config.ts
@@ -1,33 +1,40 @@
 export default {
-    // https://stackoverflow.com/questions/79470698/with-syncpack-on-a-turborepo-how-to-set-app-version-dependencies-through-a-packa/79477388#79477388
-    versionGroups: [
-        {
-            label: "use workspace protocol for local dependencies",
-            packages: ["**"],
-            dependencies: ["$LOCAL"],
-            dependencyTypes: ["!local"],
-            pinVersion: "workspace:*",
-        },
-    ],
-    // https://jamiemason.github.io/syncpack/semver-groups/
-    semverGroups: [
-        {
-            dependencyTypes: [
-                "prod",
-                "resolutions",
-                "overrides",
-                "pnpmOverrides",
-                "local",
-            ],
-            range: "^",
-        },
-        {
-            dependencyTypes: ["dev"],
-            range: "^",
-        },
-        {
-            dependencyTypes: ["peer"],
-            range: "^",
-        },
-    ],
+  // https://stackoverflow.com/questions/79470698/with-syncpack-on-a-turborepo-how-to-set-app-version-dependencies-through-a-packa/79477388#79477388
+  versionGroups: [
+    {
+      label: "use workspace protocol for local dependencies",
+      packages: ["**"],
+      dependencies: ["$LOCAL"],
+      dependencyTypes: ["!local"],
+      pinVersion: "workspace:*",
+    },
+  ],
+  // https://jamiemason.github.io/syncpack/semver-groups/
+  semverGroups: [
+    {
+      // "highly recommended to save the exact version"
+      // https://biomejs.dev/internals/versioning/
+      dependencies: ["@biomejs/biome"],
+      dependencyTypes: ["dev"],
+      range: "",
+    },
+    {
+      dependencyTypes: [
+        "prod",
+        "resolutions",
+        "overrides",
+        "pnpmOverrides",
+        "local",
+      ],
+      range: "^",
+    },
+    {
+      dependencyTypes: ["dev"],
+      range: "^",
+    },
+    {
+      dependencyTypes: ["peer"],
+      range: "^",
+    },
+  ],
 } satisfies import("syncpack").RcFile;
