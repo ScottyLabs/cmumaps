69641ac71c9d755618f683d718ff3b5c6a0e147d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-09 18:02:34 -0500

    chore: keep comments when generating biome config

--- scripts/biome/write-config.ts
+++ scripts/biome/write-config.ts
@@ -1,10 +1,9 @@
-import fs from "node:fs/promises";
+import fs, { writeFile } from "node:fs/promises";
+import { parse, stringify, type CommentObject } from "comment-json";
 import { BIOME_CONFIG_PATH, getAllRules } from "./generate-rules.ts";
 
 const allRules = await getAllRules();
-const biomeConfig = JSON.parse(await fs.readFile(BIOME_CONFIG_PATH, "utf-8"));
+const biomeConfig = parse(await fs.readFile(BIOME_CONFIG_PATH, "utf-8"), null) as CommentObject;
+// @ts-expect-error
 biomeConfig.linter.rules = allRules;
-await fs.writeFile(
-  BIOME_CONFIG_PATH,
-  `${JSON.stringify(biomeConfig, null, 2)}\n`,
-);
+await writeFile(BIOME_CONFIG_PATH, `${stringify(biomeConfig, null, 2)}\n`);
