fb3d5d5c845c58bee344f0be825ddec4a06e516c
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-09 00:13:40 -0500

    refactor: remove get_clerk_token file

--- apps/dataflow/src/clients/api_client.py
+++ apps/dataflow/src/clients/api_client.py
@@ -2,8 +2,6 @@ import os
 
 from clerk_backend_api import Clerk
 
-from clients.get_clerk_token import TOKEN_ENV_KEY
-
 
 class ApiClient:
     def __init__(self) -> None:
@@ -17,16 +15,11 @@ class ApiClient:
     def get_token(self) -> str:
         if self._cached_token:
             return self._cached_token
-        if TOKEN_ENV_KEY in os.environ:
-            self._cached_token = os.environ[TOKEN_ENV_KEY]
-            return self._cached_token
+
         with Clerk(bearer_auth=self.machine_secret) as clerk:
             res = clerk.m2m.create_token()
             token = res.token
 
             # Cache in memory
             self._cached_token = token
-            # Optionally cache in environment (process-level only)
-            os.environ[self.token_env_key] = token
-
             return token
