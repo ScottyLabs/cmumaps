6568c3d19688c0b86ad28377aebe484b84585c3d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-08 22:38:53 -0500

    chore: test logger message colors in Railway

--- apps/dataflow/src/_25live/main.py
+++ apps/dataflow/src/_25live/main.py
@@ -3,4 +3,11 @@ from logger.app_logger import get_app_logger
 
 def main() -> None:
     app_logger = get_app_logger()
-    app_logger.print("Hello, World!")
+    app_logger.print("Print message")
+    app_logger.print_bold("Print bold message")
+    app_logger.success("Success message")
+    app_logger.debug("Debug message")
+    app_logger.info("Info message")
+    app_logger.warning("Warning message")
+    app_logger.error("Error message")
+    app_logger.critical("Critical message")
