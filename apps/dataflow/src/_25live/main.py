58b71f82ea715b5029e018d285fed17e97737160
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-08 23:09:47 -0500

    chore: test default logging methods first
    Shows {
      "message": "\u001b[0m",
      "attributes": {
        "level": "info"
      },
    
    In railway?

--- apps/dataflow/src/_25live/main.py
+++ apps/dataflow/src/_25live/main.py
@@ -3,10 +3,10 @@ from logger.app_logger import get_app_logger
 
 def main() -> None:
     app_logger = get_app_logger()
-    app_logger.print("Print message")
-    app_logger.success("Success message")
     app_logger.debug("Debug message")
     app_logger.info("Info message")
     app_logger.warning("Warning message")
     app_logger.error("Error message")
     app_logger.critical("Critical message")
+    app_logger.print("Print message")
+    app_logger.success("Success message")
