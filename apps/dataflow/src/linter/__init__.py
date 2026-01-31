909917a8379a4b692169aa1f1abc4fbc71f21212
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-30 20:30:20 -0500

    chore: move mypy strict flag to pyproject

--- apps/dataflow/src/linter/__init__.py
+++ apps/dataflow/src/linter/__init__.py
@@ -1,3 +1,4 @@
+
 """Linter for the dataflow application."""
 
 import subprocess
@@ -6,5 +7,5 @@ import subprocess
 def main() -> None:
     """For uv run lint to work."""
     subprocess.run(["ruff", "check"], check=True)  # noqa: S607
-    subprocess.run(["mypy", ".", "--strict"], check=True)  # noqa: S607
+    subprocess.run(["mypy", "."], check=True)  # noqa: S607
     subprocess.run(["ty", "check"], check=True)  # noqa: S607
