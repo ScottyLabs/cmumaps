58ed9cd243b16fc1de7370316ee24eb3186bd595
Anish Pallati <i@anish.land>
2026-04-27 00:50:49 -0400

    chore: delete biome

--- scripts/biome/write-config.ts
+++
@@ -1,9 +0,0 @@
-import fs, { writeFile } from "node:fs/promises";
-import { parse, stringify } from "comment-json";
-import { BIOME_CONFIG_PATH, generateRules } from "./generate-rules.ts";
-
-const generatedRules = await generateRules();
-const biomeConfig = parse(await fs.readFile(BIOME_CONFIG_PATH, "utf-8"));
-// @ts-expect-error
-biomeConfig.linter.rules = generatedRules;
-await writeFile(BIOME_CONFIG_PATH, `${stringify(biomeConfig, null, 2)}\n`);
