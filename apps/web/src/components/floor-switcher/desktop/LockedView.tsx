85af488753d77ee243a85f80e6677c88d61f77a1
Anish Pallati <i@anish.land>
2026-04-27 16:17:44 -0400

    fix(web): constrain locked view icon size

--- apps/web/src/components/floor-switcher/desktop/LockedView.tsx
+++ apps/web/src/components/floor-switcher/desktop/LockedView.tsx
@@ -8,8 +8,8 @@ interface Props {
 const LockedView = ({ building }: Props) => (
   <div className="flex items-center">
     <p className="mr-4 ml-2">{building.name}</p>
-    <div className="flex items-center rounded-r bg-gray-200 p-2">
-      <img alt="Lock Icon" src={lockIcon} />
+    <div className="flex items-center self-stretch rounded-r bg-gray-200 px-2.5">
+      <img alt="Lock Icon" src={lockIcon} className="h-6 w-auto" />
     </div>
   </div>
 );
